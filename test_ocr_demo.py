020bc2dafcb43449cded311e773226a09bd102b3
Erin Liang <ell2147@columbia.edu>
2022-03-24 02:32:08 -0400

    added interactive thresholding

--- test_ocr_demo.py
+++ test_ocr_demo.py
@@ -14,17 +14,17 @@ from PIL import Image
 import pytesseract
 import cv2
 
-
 # site copy
-MAIN_TITLE_TEXT = 'Inspecting Text Quality of OCR\n'
+MAIN_TITLE_TEXT = 'Inspecting OCR Quality\n'
 TITLE_DESCRIPTION = '*Built for http://history-lab.org/*\n'
+THRESHOLD_HELP = "If the pixel value is smaller than this number, it is set to 0"
+MAXTHRESHOLD_HELP = "If the pixel value is larger than the threshold, it is set to this number"
 
+THRESHOLDING_METHODS = ["Simple", "Adaptive", "Otsu"]
 DET_ARCHS = ['Tesseract', 'Other OCR Method to test', 'EasyOCR']
-NER_ARCHS = ["filler1", "filler2", "filler3"]
 
 # testing
 TEST_IMAGE_PATH = "test_images/plain_text.png"
-
 
 def main():
 	# Wide mode
@@ -34,26 +34,26 @@ def main():
 
 	# File selection UI
 	st.sidebar.title("File Selection")
-	st.subheader("Uploaded Image")
-	uploaded_file = st.sidebar.file_uploader("Upload a File",type=None)
+	uploaded_file = st.sidebar.file_uploader("Upload a File Containing Text",type=None)
 	st.set_option('deprecation.showfileUploaderEncoding', False) # Disabling warning
 
-	# TODO: find min_value, max_value of the file,
 	st.sidebar.subheader("Enter Page Range")
 	col1, col2 = st.sidebar.columns(2)
 	fpage = col1.text_input("First Page")
 	lpage = col2.text_input("Last Page")
-	# fpage, lpage = "blah", "blah"
 
 	if fpage != "" and lpage != "" and uploaded_file is not None:
 		uploaded_file_copy = copy.copy(uploaded_file) # PyPDF2 malforms input pdf, need to copy
 		if not isRangeValid(int(fpage), int(lpage), countNumPages(uploaded_file_copy)):
 			st.sidebar.error("Out of bounds page reference, limit is blah")
 	
-	# Model selection -- setting up UI for future use
-	st.sidebar.title("Model selection")
-	ner_arch = st.sidebar.selectbox("NER Model", NER_ARCHS)
-	button = st.sidebar.button("Extract Text")
+	st.sidebar.title("Pre-Processing Parameters")
+	thresh_method = st.sidebar.radio("Thresholding Method", THRESHOLDING_METHODS)
+	if thresh_method == "Simple":
+				thresh_val = st.sidebar.slider("Threshold Value", value=127, min_value=50, max_value=300, help=THRESHOLD_HELP)
+				max_val = st.sidebar.slider("Maximum Value", value=255, min_value=100, max_value=1000, help=MAXTHRESHOLD_HELP)
+
+	st.sidebar.title("Tesseract Parameters")
 
 	if uploaded_file is None or fpage == "" or lpage == "":
 		st.info('Please upload a file')
@@ -61,34 +61,38 @@ def main():
 		st.info('Please Upload a file')
 
 	else: # use uploaded a file
-		with st.container():
+		st.subheader("Uploaded File")
+		with st.expander("File", expanded=True):
+
 			# uploaded pdf -> temporary file -> image
 			tfile = tempfile.NamedTemporaryFile(delete=False)
 			tfile.write(uploaded_file.read())
 			image = convert_from_path(tfile.name, first_page=int(fpage), last_page=int(lpage)) # in-memory image
 
-			# select a range of pages to do text processing on
-			# image[0]
-
 			# display
 			st.image(image)
-			# st.write(pdf_html, unsafe_allow_html=True)
-			st.markdown("---")
+		st.markdown("---")
 
 		# attempt to extract text
-		st.subheader("Text Extracted")
-		test_extracted_expander = st.expander("Text from Image", expanded=True)
-		with test_extracted_expander:
+		st.subheader("Image Pre-Processing")
+		with st.expander("Pre-Processing Results", expanded=True):
 
 			# grayscale image to improve processing
 			# for image files
-			# img = Image.open(timage.name).convert("L")
-			# ret,img = cv2.threshold(np.array(img), 90, 400, cv2.THRESH_BINARY)
-
+			# res
 			# testing
-			ret,img = cv2.threshold(np.array(image[0]), 90, 400, cv2.THRESH_BINARY)
+			
+			button = st.sidebar.button("Extract Text")
+			ret,img = cv2.threshold(np.array(image[0]), thresh_val, max_val, cv2.THRESH_BINARY)
 			img = Image.fromarray(img.astype(np.uint8))
 			st.image(img) # display grayscaled image
+		st.markdown("---")
+
+
+		
+		st.subheader("Text Extraction")
+		test_extracted_expander = st.expander("Text from Pre-Processed Image", expanded=True)
+		with test_extracted_expander:
 
 			extracted_text = pytesseract.image_to_string(img, config='--psm 7 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz, 0123456789.%', lang="eng")
 
