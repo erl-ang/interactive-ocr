c7539aa6579474757d92f50e130257a8f151a9ff
Erin Liang <ell2147@columbia.edu>
2022-12-16 08:02:38 -0500

    fixing langid, adding mean word conf from pytesseract

--- lang_confidence/lang_id.py
+++ lang_confidence/lang_id.py
@@ -3,5 +3,4 @@ import os
 
 def getConf(): 
     data = run("./lang_confidence/scorelines.sh ./ocr_text.txt | ./lang_confidence/ocrquality.rb",capture_output=True,shell=True)
-    print(data.stderr)
     return data.stdout
